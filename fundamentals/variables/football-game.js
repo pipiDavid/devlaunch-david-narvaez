49abdb7d07ecd0b7a8444ead4ef9711a859bd25e
Jose David Narvaez <josedavidnarvaezsoto@gmail.com>
2025-01-13 20:43:22 -0600

    chore(variables): add football game exercise

---
+++ fundamentals/variables/football-game.js
@@ -0,0 +1,24 @@
+
+const eventName = 'Eliminatorias Sudamericanas' 
+
+const date = '22/11/23'
+
+const time = '20:30'
+
+const localTeam = 'Argentina'
+const awayTeam = 'Brasil'
+
+const localTeamScore = 1
+const awayTeamScore = 0
+
+const score = `${localTeamScore}-${awayTeamScore}`
+
+console.log(`
+    Torneo: ${eventName} (${time})
+
+    Equipos: ${localTeam}-${awayTeam}
+    
+    Resultado: ${score}
+    `)
+
+

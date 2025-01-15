277443860ee76d36ee07c1a9a1024613405aa900
Jose David Narvaez <josedavidnarvaezsoto@gmail.com>
2025-01-14 19:28:45 -0600

    chore(variables): add supermarket exercise

---
+++ fundamentals/variables/supermarket.js
@@ -0,0 +1,25 @@
+const superMarket = 'GreenCenter'
+
+const favoriteFruit1 = 'manzanas'
+const favoriteFruit2 = 'naranjas'
+
+const areBananasAvailable = false
+const areGrapesAvailable = false
+
+const numberOfActiveCashier = 3
+
+const myCash = 150
+const total = 100
+const change = myCash - total
+
+console.log(`
+     
+    SuperMarket: ${superMarket}
+
+    Payment: ${150}
+
+    Total: ${100}
+    
+    Change: ${change}
+
+    `)

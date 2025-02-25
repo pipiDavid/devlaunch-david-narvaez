210caaf4dbf546754cfc402599f67cc03cf13c04
Jose David Narvaez <josedavidnarvaezsoto@gmail.com>
2025-02-24 19:46:40 -0600

    chore(loops): add classify-numbers exercise

---
+++ data-structures/loops/classify-numbers.ts
@@ -0,0 +1,12 @@
+const classifyNumber = (num: number): string => {
+    let result = ''
+  
+    for (let i = 1; i <= num; i++) {
+      const isOdd = i % 2 === 0
+      result += ` ${i}: is ${isOdd ? 'odd' : 'even'}\n `
+    }
+    return result
+  }
+  
+  console.log(classifyNumber(10))
+  

adedab05a9449715b264da0a1f40b4ba55dd9565
Jose David Narvaez <josedavidnarvaezsoto@gmail.com>
2025-08-12 16:49:37 -0600

    feat(jest): add jest.config file

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts']
 };
